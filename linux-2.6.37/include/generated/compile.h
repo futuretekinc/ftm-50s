a8467ae5fdbdde7fac20354f3e6710812488f99d
Inhyun Cho <xtra72@gmail.com>
2014-11-11 02:59:41 -0800

    임시 생성 파일들 삭제

--- linux-2.6.37/include/generated/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 7 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 Wed Oct 29 19:57:59 PDT 2014"
-#define LINUX_COMPILE_TIME "19:57:59"
-#define LINUX_COMPILE_BY "xtra"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.7.2 20130102 (STMicroelectronics/Linux Base 4.7.2-120) (GCC) "
