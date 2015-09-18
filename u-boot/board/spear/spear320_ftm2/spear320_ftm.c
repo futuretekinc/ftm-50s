498302d17607a43333962146f5499cde83307ed9
Inhyun Cho <xtra72@gmail.com>
2015-09-18 15:46:13 +0900

    add ftm2 for SSLEdge

---
+++ u-boot/board/spear/spear320_ftm2/spear320_ftm.c
@@ -0,0 +1,269 @@
+/*
+ * (C) Copyright 2009
+ * Ryan Chen, ST Micoelectronics, ryan.chen@st.com.
+ * Vipin Kumar, ST Micoelectronics, vipin.kumar@st.com.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <nand.h>
+#include <asm/io.h>
+#include <linux/mtd/fsmc_nand.h>
+#include <asm/arch/hardware.h>
+#include <asm/arch/spr_defs.h>
+#include <asm/arch/spr_misc.h>
+
+#define PLGPIO_SEL_6_9	0xb3000024
+#define PLGPIO_IO_6_9	0xb3000034
+#define PLGPIO_SEL_31	0xb3000024
+#define PLGPIO_IO_31	0xb3000034
+#define PLGPIO_SEL_35	0xb3000028
+#define PLGPIO_IO_35	0xb3000038
+#define PLGPIO_SEL_36	0xb3000028
+#define PLGPIO_IO_36	0xb3000038
+#define PLGPIO_SEL_39	0xb3000028
+#define PLGPIO_IO_39	0xb3000038
+#define PLGPIO_SEL_41	0xb3000028
+#define PLGPIO_IO_41	0xb3000038
+#define PLGPIO_SEL_42	0xb3000028
+#define PLGPIO_IO_42	0xb3000038
+#define PLGPIO_SEL_46	0xb3000028
+#define PLGPIO_IO_46	0xb3000038
+#define PLGPIO_SEL_69	0xb300002C
+#define PLGPIO_IO_69	0xb300003C
+#define PLGPIO_SEL_70	0xb300002C
+#define PLGPIO_IO_70	0xb300003C
+#define PLGPIO_SEL_73	0xb300002C
+#define PLGPIO_IO_73	0xb300003C
+#define PLGPIO_SEL_76	0xb300002C
+#define PLGPIO_IO_76	0xb300003C
+#define PLGPIO_31	(0x1 << 31)
+#define PLGPIO_35	(0x1 << 3)
+#define PLGPIO_36	(0x1 << 4)
+#define PLGPIO_41	(0x1 << 9)
+#define PLGPIO_42	(0x1 << 10)
+#define PLGPIO_69	(0x1 << 5)
+#define PLGPIO_70	(0x1 << 6)
+#define PLGPIO_73	(0x1 << 9)
+#define PLGPIO_76	(0x1 << 12)
+#define PLGPIO_6_9	((0x1 << 6) | (0x1 << 9))
+
+#define	PORT_ID(x)	((x) + 8)
+
+static void GPS_reset(void)
+{
+	/* PLGPIO73 is used to reset GPS module */
+	writel(readl(PLGPIO_IO_73) & ~PLGPIO_73, PLGPIO_IO_73);
+	writel(readl(PLGPIO_SEL_73) | PLGPIO_73, PLGPIO_SEL_73);
+	writel(readl(PLGPIO_IO_73) | PLGPIO_73, PLGPIO_IO_73);
+}
+
+static void phy_reset(void)
+{
+	/* PLGPIO76 is used to reset phy */
+	writel(readl(PLGPIO_IO_6_9) & ~PLGPIO_6_9, PLGPIO_IO_6_9);
+	writel(readl(PLGPIO_SEL_6_9) | PLGPIO_6_9, PLGPIO_SEL_6_9);
+	writel(readl(PLGPIO_IO_6_9) | PLGPIO_6_9, PLGPIO_IO_6_9);
+}
+
+static void NF_write_protect_off(void)
+{
+	/* PLGPIO69 is used to write protection off of nand flash */
+	writel(readl(PLGPIO_IO_69) | PLGPIO_69, PLGPIO_IO_69);
+	writel(readl(PLGPIO_SEL_69) | PLGPIO_69, PLGPIO_SEL_69);
+}
+
+static void telit_init(void)
+{
+	writel(readl(PLGPIO_IO_35) & ~PLGPIO_35, PLGPIO_IO_35);
+	writel(readl(PLGPIO_SEL_35) | PLGPIO_35, PLGPIO_SEL_35);
+	writel(readl(PLGPIO_IO_36) | PLGPIO_36, PLGPIO_IO_36);
+	writel(readl(PLGPIO_SEL_36) | PLGPIO_36, PLGPIO_SEL_36);
+	writel(readl(PLGPIO_IO_41) | PLGPIO_41, PLGPIO_IO_41);
+	writel(readl(PLGPIO_SEL_41) | PLGPIO_41, PLGPIO_SEL_41);
+	writel(readl(PLGPIO_IO_42) | PLGPIO_42, PLGPIO_IO_42);
+	writel(readl(PLGPIO_SEL_42) | PLGPIO_42, PLGPIO_SEL_42);
+	writel(readl(PLGPIO_IO_70) & ~PLGPIO_70, PLGPIO_IO_70);
+	writel(readl(PLGPIO_SEL_70) | PLGPIO_70, PLGPIO_SEL_70);
+	
+}
+
+int	telit_stat(void)
+{
+	static int stat = 0;
+	if (stat)
+	{
+		writel(readl(PLGPIO_IO_35) | PLGPIO_35, PLGPIO_IO_35);
+		writel(readl(PLGPIO_SEL_35) | PLGPIO_35, PLGPIO_SEL_35);
+	}
+	else
+	{
+		writel(readl(PLGPIO_IO_35) & ~PLGPIO_35, PLGPIO_IO_35);
+		writel(readl(PLGPIO_SEL_35) | PLGPIO_35, PLGPIO_SEL_35);
+	}
+
+	stat = !stat;
+}
+
+int	mv88e6060_init(void)
+{
+
+}
+
+int board_init(void)
+{
+	writel(readl(0xB300000C) & ~(1 << 11), 0xB300000C);
+	writel(readl(0xB3000024) | (1 << 7), 0xB3000024);
+	writel(readl(0xB3000044) | (1 << 7), 0xB3000044);
+
+	writel(readl(0xB3000028) | (1 << 7), 0xB3000028);
+	writel(readl(0xB3000048) | (1 << 7), 0xB3000048);
+	writel(readl(0xB3000028) | (1 << 14), 0xB3000028);
+	writel(readl(0xB3000038) & !(1 << 14), 0xB3000038);
+	writel(readl(0xB3000048) & !(1 << 14), 0xB3000048);
+
+	GPS_reset();
+	phy_reset();
+	NF_write_protect_off();
+	telit_init();
+
+	return spear_board_init(MACH_TYPE_SPEAR320_FTM);
+}
+
+/*
+ * board_nand_init - Board specific NAND initialization
+ * @nand:	mtd private chip structure
+ *
+ * Called by nand_init_chip to initialize the board specific functions
+ */
+
+int board_nand_init(struct nand_chip *nand)
+{
+#if defined(CONFIG_NAND_FSMC)
+	int	ret;
+	struct misc_regs *const misc_regs_p =
+	    (struct misc_regs *)CONFIG_SPEAR_MISCBASE;
+
+	if (((readl(&misc_regs_p->auto_cfg_reg) & MISC_SOCCFGMSK) ==
+	     MISC_SOCCFG30) ||
+	    ((readl(&misc_regs_p->auto_cfg_reg) & MISC_SOCCFGMSK) ==
+	     MISC_SOCCFG31)) {
+
+		board_power_hold(1);
+		ret = fsmc_nand_init(nand);
+		board_power_hold(0);
+
+		return	ret;
+	}
+#endif
+	return -1;
+}
+
+int board_eth_init(bd_t *bis)
+{
+	int i;
+	unsigned short reg_short = 0x8000;
+
+#if defined(CONFIG_MACB)
+	struct misc_regs *const misc_regs_p =
+		(struct misc_regs *)CONFIG_SPEAR_MISCBASE;
+#endif
+	int ret = 0;
+#if defined(CONFIG_DESIGNWARE_ETH)
+	u32 interface = PHY_INTERFACE_MODE_MII;
+	if (designware_initialize(0, CONFIG_SPEAR_ETHBASE, CONFIG_DW0_PHY,
+				interface) >= 0)
+		ret++;
+#endif
+#if defined(CONFIG_MACB)
+	/* Enable AMEM clock for memory port access */
+	writel(readl(&misc_regs_p->amem_cfg_ctrl) | 0x1,
+			&misc_regs_p->amem_cfg_ctrl);
+
+	if (macb_eth_initialize(0, (void *)CONFIG_SYS_MACB1_BASE,
+				CONFIG_MACB0_PHY) >= 0)
+		ret++;
+#endif
+
+
+	miiphy_write("mii0", 0x0F, 0x0A, 0x8000);
+
+	reg_short = 0x8000;
+	while(reg_short & 0x8000)
+	{
+		miiphy_read("mii0", 0x0F, 0x0A, &reg_short);
+	}
+
+	miiphy_read("mii0", 0x08, 0x03, &reg_short) ;
+
+	for(i = 0 ; i < 4 ; i++)
+	{
+		miiphy_write("mii0", i, PHY_BMCR, PHY_BMCR_RESET);
+		reg_short = PHY_BMCR_RESET;
+
+		while (reg_short & PHY_BMCR_RESET)
+		{
+			miiphy_read("mii0", i, PHY_BMCR, &reg_short);// Be here until Phy_Reset bit gets clear
+		}
+	}
+
+	for(i = 0 ; i < 6 ; i++)
+	{
+		miiphy_read("mii0", PORT_ID(i), 0x04, &reg_short);
+		miiphy_write("mii0", PORT_ID(i), 0x04, reg_short & ~0x0003);
+	}
+
+	miiphy_write("mii0", PORT_ID(0), 0x06, 0x0010);
+	miiphy_write("mii0", PORT_ID(1), 0x06, 0x002C);
+	miiphy_write("mii0", PORT_ID(2), 0x06, 0x002A);
+	miiphy_write("mii0", PORT_ID(3), 0x06, 0x0026);
+	miiphy_write("mii0", PORT_ID(4), 0x06, 0x0001);
+	miiphy_write("mii0", PORT_ID(5), 0x06, 0x000E);
+
+	for(i = 0 ; i < 6 ; i++)
+	{
+		miiphy_read("mii0", PORT_ID(i), 0x04, &reg_short);
+		miiphy_write("mii0", PORT_ID(i), 0x04, reg_short | 0x0003);
+	}
+
+	for(i = 0 ; i < 4 ; i++)
+	{
+		miiphy_write("mii0", i, 0x16, 0x4A35);
+	}
+
+	return ret;
+}
+
+int board_power_hold(int on)
+{
+	if (on)
+	{
+		writel(readl(0xB3000038) | (1 << 14), 0xB3000038);
+	}
+	else
+	{
+		writel(readl(0xB3000038) & ~(1 << 14), 0xB3000038);
+	}
+
+	return	0;
+	
+}
---
+++ u-boot/include/configs/spear3xx_ftm2.h
@@ -0,0 +1,249 @@
+/*
+ * (C) Copyright 2009
+ * Vipin Kumar, STMicroelectronics, <vipin.kumar@st.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+/*
+ * High Level Configuration Options
+ * (easy to change)
+ */
+#if defined(CONFIG_MK_spear300)
+#define CONFIG_SPEAR3XX				1
+#define CONFIG_SPEAR300				1
+#elif defined(CONFIG_MK_spear310)
+#define CONFIG_SPEAR3XX				1
+#define CONFIG_SPEAR310				1
+#elif defined(CONFIG_MK_spear320)
+	#if defined(CONFIG_MK_hmi)
+	#define CONFIG_SPEAR3XX			1
+	#define CONFIG_SPEAR320_HMI		1
+	#elif defined(CONFIG_MK_ftm)
+	#define CONFIG_SPEAR3XX			1
+	#define CONFIG_SPEAR320_FTM		1
+	#else
+	#define CONFIG_SPEAR3XX			1
+	#define CONFIG_SPEAR320 		1
+	#endif
+#endif
+
+#define CONFIG_ENV_IS_IN_FLASH			1
+
+#if defined(CONFIG_MK_usbtty)
+#define CONFIG_SPEAR_USBTTY			1
+
+/*
+ * To support saveenv command
+ */
+#endif
+
+#include <configs/spear-common.h>
+
+#define CONFIG_SYS_INFO_BASE	0xF87E0000
+
+#undef	CONFIG_USB_STORAGE
+
+#if !defined(CONFIG_SPEAR_USBTTY)
+/* Ethernet driver configuration */
+#define CONFIG_DW_ALTDESCRIPTOR			1
+
+#if defined(CONFIG_SPEAR310)
+#define CONFIG_ETH_MDIO_HOOK			1
+#define CONFIG_MACB				1
+#define CONFIG_MACB0_PHY			0x01
+#define CONFIG_MACB1_PHY			0x03
+#define CONFIG_MACB2_PHY			0x05
+#define CONFIG_MACB3_PHY			0x07
+
+#elif defined(CONFIG_SPEAR320)
+#define CONFIG_ETH_MDIO_HOOK			1
+#define CONFIG_MACB				1
+#define CONFIG_MACB0_PHY			0x01
+#define CONFIG_MACB1_PHY			0x02
+
+#elif defined(CONFIG_SPEAR320_HMI)
+#define CONFIG_ETH_MDIO_HOOK			1
+#define CONFIG_MACB				1
+#define CONFIG_MACB0_PHY			0x1
+#define CONFIG_MACB1_PHY			0x0
+
+#elif defined(CONFIG_SPEAR320_FTM)
+#define CONFIG_ETH_MDIO_HOOK			1
+#define CONFIG_MACB				1
+#define CONFIG_MACB0_PHY			0x00
+#define CONFIG_MACB1_PHY			0x01
+
+#endif
+#endif
+
+#define CONFIG_ETHADDR		00:40:5c:01:01:01
+#define CONFIG_ETH1ADDR		00:40:5c:01:01:02
+#define CONFIG_NETMASK		255.255.255.0
+#define CONFIG_IPADDR		10.0.1.125
+#define CONFIG_SERVERIP		10.0.1.101
+#define CONFIG_GATEWAYIP	10.0.1.1
+
+/* Serial Configuration (PL011) */
+#define CONFIG_SYS_SERIAL0			0xD0000000
+
+#if defined(CONFIG_SPEAR300)
+#define CONFIG_PL01x_PORTS			{(void *)CONFIG_SYS_SERIAL0}
+
+#elif defined(CONFIG_SPEAR310)
+
+#if (CONFIG_CONS_INDEX)
+#undef  CONFIG_PL011_CLOCK
+#define CONFIG_PL011_CLOCK			(83 * 1000 * 1000)
+#endif
+
+#define CONFIG_SYS_SERIAL1			0xB2000000
+#define CONFIG_SYS_SERIAL2			0xB2080000
+#define CONFIG_SYS_SERIAL3			0xB2100000
+#define CONFIG_SYS_SERIAL4			0xB2180000
+#define CONFIG_SYS_SERIAL5			0xB2200000
+#define CONFIG_PL01x_PORTS			{(void *)CONFIG_SYS_SERIAL0, \
+						(void *)CONFIG_SYS_SERIAL1, \
+						(void *)CONFIG_SYS_SERIAL2, \
+						(void *)CONFIG_SYS_SERIAL3, \
+						(void *)CONFIG_SYS_SERIAL4, \
+						(void *)CONFIG_SYS_SERIAL5 }
+#elif defined(CONFIG_SPEAR320) || defined(CONFIG_SPEAR320_HMI) || defined(CONFIG_SPEAR320_FTM)
+
+#if (CONFIG_CONS_INDEX)
+#undef  CONFIG_PL011_CLOCK
+#define CONFIG_PL011_CLOCK			(83 * 1000 * 1000)
+#endif
+
+#define CONFIG_SYS_SERIAL1			0xA3000000
+#define CONFIG_SYS_SERIAL2			0xA4000000
+#define CONFIG_PL01x_PORTS			{(void *)CONFIG_SYS_SERIAL0, \
+						(void *)CONFIG_SYS_SERIAL1, \
+						(void *)CONFIG_SYS_SERIAL2 }
+#endif
+
+#if defined(CONFIG_SPEAR_EMI)
+
+#define CONFIG_SYS_FLASH_CFI
+#define CONFIG_FLASH_CFI_DRIVER
+
+#if defined(CONFIG_SPEAR310)
+#define CONFIG_SYS_FLASH_PROTECTION		1
+#define CONFIG_SYS_FLASH_BASE			0x50000000
+#define CONFIG_SYS_CS1_FLASH_BASE		0x60000000
+#define CONFIG_SYS_CS2_FLASH_BASE		0x70000000
+#define CONFIG_SYS_CS3_FLASH_BASE		0x80000000
+#define CONFIG_SYS_CS4_FLASH_BASE		0x90000000
+#define CONFIG_SYS_CS5_FLASH_BASE		0xA0000000
+#define CONFIG_SYS_FLASH_BANKS_LIST		{ CONFIG_SYS_FLASH_BASE,   \
+						CONFIG_SYS_CS1_FLASH_BASE, \
+						CONFIG_SYS_CS2_FLASH_BASE, \
+						CONFIG_SYS_CS3_FLASH_BASE, \
+						CONFIG_SYS_CS4_FLASH_BASE, \
+						CONFIG_SYS_CS5_FLASH_BASE }
+#define CONFIG_SYS_MAX_FLASH_BANKS		6
+
+#elif defined(CONFIG_SPEAR320) || defined(CONFIG_SPEAR320_HMI) || defined(CONFIG_SPEAR320_FTM)
+#define CONFIG_SYS_FLASH_PROTECTION		1
+#define CONFIG_SYS_FLASH_BASE			0x44000000
+#define CONFIG_SYS_CS1_FLASH_BASE		0x45000000
+#define CONFIG_SYS_CS2_FLASH_BASE		0x46000000
+#define CONFIG_SYS_CS3_FLASH_BASE		0x47000000
+#define CONFIG_SYS_FLASH_BANKS_LIST		{ CONFIG_SYS_FLASH_BASE,   \
+						CONFIG_SYS_CS1_FLASH_BASE, \
+						CONFIG_SYS_CS2_FLASH_BASE, \
+						CONFIG_SYS_CS3_FLASH_BASE }
+#define CONFIG_SYS_MAX_FLASH_BANKS		4
+
+#endif
+
+#define CONFIG_SYS_MAX_FLASH_SECT		(127 + 8)
+#define CONFIG_SYS_FLASH_QUIET_TEST		1
+
+#endif
+
+/* NAND flash configuration */
+#define CONFIG_SYS_FSMC_NAND_8BIT		1
+
+//#define CONFIG_MTD_DEBUG        1
+//#define CONFIG_MTD_DEBUG_VERBOSE 3
+
+#define	CONFIG_CMD_RECOVERY
+#define	CONFIG_CMD_POWER
+
+#if defined(CONFIG_SPEAR300)
+#define CONFIG_SYS_NAND_BASE			(0x80000000)
+
+#elif defined(CONFIG_SPEAR310)
+#define CONFIG_SYS_NAND_BASE			(0x40000000)
+
+#elif defined(CONFIG_SPEAR320) || defined(CONFIG_SPEAR320_HMI) || defined(CONFIG_SPEAR320_FTM)
+#define CONFIG_SYS_NAND_BASE			(0x50000000)
+
+#endif
+
+#undef	CONFIG_EXTRA_ENV_SETTINGS		
+#define	CONFIG_EXTRA_ENV_SETTINGS	\
+	"model=FTM-50S\0"\
+	"devid=FTM-50S01A0001\0"\
+	"flashfiles=ftm-50s-new/\0"\
+	"loader_name=u-boot.img\0"\
+	"loader_loc=0xf8010000\0"\
+	"kernel_name=uImage\0"\
+	"kernel_loc=0xf8050000\0"\
+	"kernel_size=0x003c0000\0"\
+	"rootfs_name=rootfs.img\0"\
+	"rootfs_loc=0xf8310000\0"\
+	"rootfs_size=0x004f0000\0"\
+	"rf_loader=tftp $(flashfiles)$(loader_name);"\
+	           "protect off 1:1-4;"\
+	           "erase 1:1-4;"\
+	           "cp.b $(fileaddr) $(loader_loc) $(filesize);"\
+	           "protect on 1:1-3\0"\
+	"rf_kernel=tftp $(flashfiles)$(kernel_name)\;"\
+			   "protect off 1:5-47\;"\
+	           "erase 1:5-47\;"\
+	           "cp.b $(fileaddr) $(kernel_loc) $(kernel_size)\;"\
+	           "protect on 1:5-47\0"\
+	"rf_rootfs=tftp $(flashfiles)$(rootfs_name)\;"\
+			   "protect off 1:49-127\;"\
+	           "erase 1:49-127\;"\
+	           "cp.b $(fileaddr) $(rootfs_loc) $(rootfs_size)\;"\
+	           "protect on 1:5-47\0"\
+	"bootargs=console=ttyS0,115200 "\
+			   "mem=128M "\
+			   "root=/dev/ram rw "\
+			   "initrd=0xa00000,32M"\
+			   "ethaddr=$(ethaddr) eth1addr=$(eth1addr) "\
+			   "model=$(model) "\
+			   "devid=$(devid)\0"
+
+#undef	CONFIG_BOOTARGS
+
+#undef	CONFIG_RAMBOOTCOMMAND
+#undef	CONFIG_NFSBOOTCOMMAND
+
+#undef	CONFIG_BOOTCOMMAND
+#define	CONFIG_BOOTCOMMAND	"bootm $(kernel_loc) $(rootfs_loc)"
+
+
+#endif  /* __CONFIG_H */
